b787aad74f0f955d36e8dbe05ebdbf029bcc9988
deelag <vladik1543u7@gmail.com>
2021-07-01 17:39:59 +0300

    Add the project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Web_Lab_1-2
+# Web Lab 1-2
+
+It's a simple responsive landing page from my IoT Web Course, written mostly in HTML and CSS.
+
+![LandingPage](images/LandingPage.gif)
---
+++ js/main.js
@@ -0,0 +1,5 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementById("header__nav").classList.toggle("change-nav");
+    document.getElementById("header__nav-bg").classList.toggle("change-nav-bg");
+}
